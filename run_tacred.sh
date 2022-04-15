894670b84b8a6018b620ee030eb989ab7b1213f1
dagservice <92561729+dagservice@users.noreply.github.com>
2022-04-15 22:41:11 +0800

    Add files via upload

--- run_tacred.sh
+++ run_tacred.sh
@@ -1,4 +1,4 @@
-for SEED in 4 11 40 42 67
-do 
+for SEED in 4 11 40 42 67;
+do
   python train_tacred.py --model_name_or_path roberta-large --data_dir ./dataset/tacred --output_dir saved_models/da-gpn-tacred-$SEED --input_format typed_entity_marker_punct --seed $SEED --run_name tacred;
 done;
