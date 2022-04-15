eaf0c3ab44e27299ae79573467b2fc660d0ecfd6
dagservice <92561729+dagservice@users.noreply.github.com>
2022-04-15 10:17:03 +0800

    Update run_tacred.sh

--- run_tacred.sh
+++ run_tacred.sh
@@ -1,4 +1,3 @@
-for SEED in 4 11 40 42 67;
-do
-  python train_tacred.py --model_name_or_path roberta-large --data_dir ./dataset/tacred --output_dir saved_models/da-gpn-tacred-$SEED --input_format typed_entity_marker_punct --seed $SEED --run_name tacred;
+for SEED in 4 11 40 42 67
+do python train_tacred.py --model_name_or_path roberta-large --data_dir ./dataset/tacred --output_dir saved_models/da-gpn-tacred-$SEED --input_format typed_entity_marker_punct --seed $SEED --run_name tacred;
 done;
